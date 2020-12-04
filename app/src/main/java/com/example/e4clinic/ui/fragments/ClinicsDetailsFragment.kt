1623cdf6a7d4c5ea71b2f4c8cb1ea864f4bf2cc9
ahmedzain958 <ahmedzain958@gmail.com>
2020-12-04 14:22:02 +0200

    feat / clinic details

--- app/src/main/java/com/example/e4clinic/ui/fragments/ClinicsDetailsFragment.kt
+++ app/src/main/java/com/example/e4clinic/ui/fragments/ClinicsDetailsFragment.kt
@@ -4,26 +4,43 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.navArgs
+import com.example.e4clinic.BR
 import com.example.e4clinic.R
+import com.example.e4clinic.databinding.FragmentClinicsDetailsBinding
+import com.example.e4clinic.ui.core.BaseFragment
+import com.example.e4clinic.ui.viewmodel.ClinicsDetailsViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
-class ClinicsDetailsFragment : Fragment() {
+@AndroidEntryPoint
+class ClinicsDetailsFragment :
+    BaseFragment<ClinicsDetailsViewModel, FragmentClinicsDetailsBinding>() {
     val args: ClinicsDetailsFragmentArgs by navArgs()
+    private val mViewModel: ClinicsDetailsViewModel by viewModels()
+    private lateinit var mViewBinding: FragmentClinicsDetailsBinding
+
+    override fun getLayoutId(): Int =
+        R.layout.fragment_clinics_details
+
+    override fun getBindingVariable(): Int = BR.viewModel
+
+    override fun getViewModel(): ClinicsDetailsViewModel = mViewModel
 
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        mViewBinding = getViewDataBinding()
+        val clinic = args.clinicArgument
+        clinic?.let {
+            mViewBinding.txtDoctorName.text = it.doctorName
+            mViewBinding.txtTime.text = it.visitTime
+            mViewBinding.txtLocation.text = it.address
+            mViewBinding.txtStatus.text = it.status
+        }
 
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        val view = inflater.inflate(R.layout.fragment_clinics_details, container, false)
-        return view
-    }
+
 
 }
